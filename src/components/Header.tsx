fb00158382988ecebe8685ba7c1e9e0218cc634a
nitty fritty <nathan.fredette@libertymutual.com>
2026-07-07 15:52:52 +0000

    Dark theme working and cleanup some .css styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,20 @@
 import { useNavigate } from "react-router-dom";
+import { useTheme } from "../context/ThemeContext";
 
 const Header = () => {
   const navigate = useNavigate();
+  const { theme, toggleTheme } = useTheme();
   return (
     <header className="site-header bg-blue-500 text-white p-4">
       <div className="w-full flex items-center justify-between">
         <h1 className="text-md font-bold text-left">Customer Manager</h1>
         <nav aria-label="Main Navigation" className="ml-auto">
-          <ul className="flex gap-3 justify-end">
+          <ul className="header-list flex gap-4">
+            <li>
+              <button onClick={toggleTheme}>
+                {theme === "light" ? "Dark Mode" : "Light Mode"}
+              </button>
+            </li>
             <li>
               <button
                 onClick={() => navigate("/")}
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,7 +1,17 @@
 import Header from "../components/Header";
 import { Outlet } from "react-router-dom";
+import { ThemeProvider, useTheme } from "../context/ThemeContext";
+import { useEffect } from "react";
 
-const Layout = () => {
+const LayoutContent = () => {
+  const { theme } = useTheme();
+
+  useEffect(() => {
+    const root = document.documentElement;
+    root.classList.remove("light", "dark");
+    root.classList.add(theme);
+  }, [theme]);
+
   return (
     <div>
       <Header />
@@ -9,6 +19,14 @@ const Layout = () => {
         <Outlet />
       </main>
     </div>
+  );
+};
+
+const Layout = () => {
+  return (
+    <ThemeProvider>
+      <LayoutContent />
+    </ThemeProvider>
   );
 };
 
