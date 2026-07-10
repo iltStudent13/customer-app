bfedd4d33b2a918017002de77f844d3b1776460c
nitty fritty <nathan.fredette@libertymutual.com>
2026-07-10 13:04:26 +0000

    Error boundary, update readme, error boundary test

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,19 @@ import AddCustomer from "./pages/AddCustomer";
 import Customers from "./pages/Customers";
 import EditCustomer from "./pages/EditCustomer";
 import Layout from "./pages/Layout";
+import ErrorBoundary from "./components/ErrorBoundary";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />}>
+      <Route
+        path="/"
+        element={
+          <ErrorBoundary fallback={<div>Something went wrong</div>}>
+            <Layout />
+          </ErrorBoundary>
+        }
+      >
         <Route index element={<Customers />} />
         <Route path="add" element={<AddCustomer />} />
         <Route path="edit/:id" element={<EditCustomer />} />
